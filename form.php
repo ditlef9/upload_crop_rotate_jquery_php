06a3a9b37be54ce0b545aa61386220244e113193
Sindre <siditlef@gmail.com>
2022-04-17 20:03:15 +0200

    m2

--- form.php
+++ form.php
@@ -40,52 +40,62 @@ if($show_header_and_footer == "1"){
 echo"
 
   <!-- HTML5 Input Form Elements -->
-<div class=\"container\">
-    <form method=\"post\" action=\"\" enctype=\"multipart/form-data\" id=\"myform\">
-        <div class='preview'>
-            <img src=\"\" id=\"img\" width=\"100\" height=\"100\">
-        </div>
-        <div >
-            <input type=\"file\" id=\"file\" name=\"file\" />
-            <input type=\"button\" class=\"button\" value=\"Upload\" id=\"but_upload\">
-        </div>
-    </form>
-</div>
+	<form method=\"post\" action=\"\" enctype=\"multipart/form-data\" id=\"myform\">
+		<div id=\"upload_feedback\"><p>Please select a file to upload</p></div>
+      	<div class='preview'>
+			<img src=\"\" id=\"img\" width=\"100\" height=\"100\">
+		</div>
+		<div>
+            	<p>
+			<input type=\"file\" id=\"file\" name=\"file\" />
+            	<input type=\"button\" class=\"button\" value=\"Upload\" id=\"but_upload\">
+            	</p>
+		</div>
+	</form>
 
-  <!-- Ajax JavaScript File Upload Logic -->
-  <script>
-  $(document).ready(function(){
 
-    $(\"#but_upload\").click(function(){
+<!-- Ajax JavaScript File Upload Logic -->
+<script>
+$(document).ready(function(){
+	$(\"#but_upload\").click(function(){
 
-        var fd = new FormData();
-        var files = $('#file')[0].files;
+		// Feedback :: Loading
+		\$(\"#upload_feedback\").html(\"<div class='info_small'><p><img src='_gfx/icons/18x18/spinner_black_18x18.png' alt='spinner_black_18x18.png' /></p></div>\");
+
+  		var fd = new FormData();
+		var files = $('#file')[0].files;
+
+
         
-        // Check file selected or not
-        if(files.length > 0 ){
-           fd.append('file',files[0]);
+		// Check file selected or not
+		if(files.length > 0 ){
+           		fd.append('file',files[0]);
 
-           $.ajax({
-              url: 'upload.php',
-              type: 'post',
-              data: fd,
-              contentType: false,
-              processData: false,
-              success: function(response){
-                 if(response != 0){
-                    $(\"#img\").attr(\"src\",response); 
-                    $(\".preview img\").show(); // Display image element
-                 }else{
-                    alert('file not uploaded');
-                 }
-              },
-           });
-        }else{
-           alert(\"Please select a file.\");
-        }
-    });
+           		$.ajax({
+              		url: 'upload.php',
+              		type: 'post',
+              		data: fd,
+              		contentType: false,
+              		processData: false,
+              		success: function(response){
+                 			if(response.charAt(0) == 1){
+                    			$(\"#img\").attr(\"src\",response); 
+                    			$(\".preview img\").show(); // Display image element
+                 			}
+					else{
+						// Feedback :: Error
+						\$(\"#upload_feedback\").html(response);
+                 			}
+          			},
+			});
+		}
+		else{
+				// Feedback :: Warning
+				\$(\"#upload_feedback\").html(\"<div class='warning_small'><p>Please select a file</p></div>\");
+		}
+	});
 });
-  </script>
+</script>
 
 
 
--- upload.php
+++ upload.php
@@ -25,31 +25,68 @@ $show_header_and_footer = "1";
 
 if(isset($_FILES['file']['name'])){
 
-   /* Getting file name */
-   $filename = $_FILES['file']['name'];
+	// Make directories
+	if(!(is_dir("$cache_path"))){
+		mkdir("$cache_path");
+	}
+	if(!(is_dir("$upload_path"))){
+		mkdir("$upload_path");
+	}
 
-   /* Location */
-   $location = "_cache/".$filename;
-   $imageFileType = pathinfo($location,PATHINFO_EXTENSION);
-   $imageFileType = strtolower($imageFileType);
+	/* Getting file name */
+	$filename = $_FILES['file']['name'];
 
-   /* Valid extensions */
-   $valid_extensions = array("jpg","jpeg","png");
+	/* Location */
+	$location = "$cache_path/".$filename;
+	$imageFileType = pathinfo($location,PATHINFO_EXTENSION);
+	$imageFileType = strtolower($imageFileType);
 
-   $response = 0;
-   /* Check file extension */
-   if(in_array(strtolower($imageFileType), $valid_extensions)) {
-      /* Upload file */
-      if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
-         $response = $location;
-      }
-   }
+	/* Valid extensions */
+	$valid_extensions = array("jpg","jpeg","png");
 
-   echo $response;
-   exit;
+	// Feedback
+	$fm_image = "?";
+	$ft_image = "";
+
+	/* Check file extension */
+	if(in_array(strtolower($imageFileType), $valid_extensions)) {
+      	/* Upload file */
+		if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
+			echo"1$location";
+			exit;
+		}
+		else{ // move uploaded file
+			switch ($_FILES["$inp_names_array[$x]"]['error']) {
+				case UPLOAD_ERR_OK:
+      				$fm_image = "There is no error, the file uploaded with success.";
+					$ft_image = "info_small";
+					break;
+				case UPLOAD_ERR_NO_FILE:
+      				// $fm_image = "no_file_uploaded";
+					$ft_image = "info_small";
+					break;
+				case UPLOAD_ERR_INI_SIZE:
+      				$fm_image = "to_big_size_in_configuration";
+					$ft_image = "info_small";
+					break;
+				case UPLOAD_ERR_FORM_SIZE:
+      				$fm_image = "to_big_size_in_form";
+					$ft_image = "info_small";
+					break;
+				default:
+      				$fm_image = "unknown_error";
+					$ft_image = "info_small";
+					break;
+			} // switch	
+		}
+	
+
+	}
+	echo"<div class=\"$fm_image\"><p>$ft_image</p></div>";
+	exit;
 }
 
-echo 0;
+echo"<div class=\"info_small\"><p>No image selected</p></div>";
 
 
 ?>
