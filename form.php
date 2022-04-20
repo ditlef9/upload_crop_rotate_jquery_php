01339a5b7826fd93facf945d9806de722a81cb9e
Sindre <sindredit@gmail.com>
2022-04-20 09:23:27 +0200

    crop

--- form.php
+++ form.php
@@ -10,9 +10,6 @@
 */
 
 /*- Form Settings --------------------------------------------------------- */
-// Paths
-$jquery_file = "_javascripts/jquery/jquery.min.js";
-
 // Config
 $show_header_and_footer = "1";
 
@@ -32,7 +29,10 @@ if($show_header_and_footer == "1"){
 	echo"	<link rel=\"stylesheet\" href=\"_gfx/file_uploader.css?size="; echo filesize("_gfx/file_uploader.css"); echo"\" type=\"text/css\" >\n";
 	echo"	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\n";
 	echo"	<meta name=\"viewport\" content=\"width=device-width; initial-scale=1.0;\"/>\n";
-	echo"	<script type=\"text/javascript\" src=\"$jquery_file\"></script>\n";
+	echo"	<script type=\"text/javascript\" src=\"_javascripts/jquery/jquery.min.js\"></script>\n";
+
+	echo"	<script src=\"_javascripts/rcrop/dist/rcrop.min.js\" ></script>\n";
+	echo"	<link href=\"_javascripts/rcrop/dist/rcrop.min.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\">\n";
 	echo"</head>\n";
 	echo"<body>\n";
 }
@@ -53,7 +53,7 @@ echo"
 			<a href=\"#\" class=\"ucrjphp_rotate\" data-deg=\"90\"><img src=\"_gfx/icons/24x24/rotate_left_outline_black_24x24.png\" alt=\"rotate_left_outline_black_24x24.png\" title=\"Roate 90&deg; left\" /></a>
 			<a href=\"#\" class=\"ucrjphp_rotate\" data-deg=\"-1\"><img src=\"_gfx/icons/24x24/rotate_right_one_outline_black_24x24.png\" alt=\"rotate_right_one_outline_black_24x24.png\" title=\"Roate 1&deg; right\" /></a>
 			<a href=\"#\" class=\"ucrjphp_rotate\" data-deg=\"1\"><img src=\"_gfx/icons/24x24/rotate_left_one_outline_black_24x24.png\" alt=\"rotate_left_one_outline_black_24x24.png\" title=\"Roate 1&deg; left\" /></a>
-			<a href=\"#\" id=\"crop\"><img src=\"_gfx/icons/24x24/crop_outline_black_24x24.png\" alt=\"crop_outline_black_24x24.png\" title=\"Crop\" /></a>
+			<a href=\"#\" id=\"ucrjphp_crop\"><img src=\"_gfx/icons/24x24/crop_outline_black_24x24.png\" alt=\"crop_outline_black_24x24.png\" id=\"ucrjphp_crop_icon\" title=\"Crop\" /></a>
 			
 			<input type=\"text\" name=\"inp_ucrjphp_image_version\" value=\"0\" size=\"1\" autocomplete=\"false\" />
 			</p>
@@ -67,8 +67,7 @@ echo"
 
 		<div class=\"ucrjphp_upload_form\">
             		<p>
-			<input type=\"file\" id=\"file\" name=\"file\" />
-            		<input type=\"button\" class=\"button\" value=\"Upload\" id=\"ucrjphp_but_upload\">
+			<input type=\"file\" id=\"ucrjphp_file\" name=\"file\" />
             		</p>
 		</div>
 	</form>
@@ -76,15 +75,16 @@ echo"
 
 
 <!-- Ajax JavaScript File Upload Logic -->
+
+
 <script>
 $(document).ready(function(){
-	$(\"#ucrjphp_but_upload\").click(function(){
-
+	\$('#ucrjphp_file').change(function(evt) {
 		// Feedback :: Loading
 		\$(\"#ucrjphp_upload_feedback\").html(\"<div class='info_small'><p><img src='_gfx/icons/18x18/spinner_black_18x18.png' alt='spinner_black_18x18.png' /> Uploading image...</p></div>\");
 
   		var fd = new FormData();
-		var files = $('#file')[0].files;
+		var files = $('#ucrjphp_file')[0].files;
 
 
         
@@ -178,7 +178,44 @@ $(document).ready(function(){
 				\$('.ucrjphp_image_preview img').attr('alt', html);
               		}
             	});
-	}); // Rotate 90
+	}); // Rotate x deg
+
+	$(\"#ucrjphp_crop\").click(function(){
+
+		// Change crop icon
+		var imagePath = \$('#ucrjphp_crop_icon').attr('src');
+		var lastIndexOf = imagePath.lastIndexOf('/');
+		var currentCropSrc = imagePath.substring(lastIndexOf + 1);
+		var cropIsActive = 1;
+		if(currentCropSrc === 'crop_outline_black_24x24.png'){
+			var cropIsActive = 1;
+                    	\$(\"#ucrjphp_crop_icon\").attr(\"src\", \"_gfx/icons/24x24/crop_remove_outline_black_24x24.png\"); 
+		}
+		else{
+			var cropIsActive = 0;
+                    	\$(\"#ucrjphp_crop_icon\").attr(\"src\", \"_gfx/icons/24x24/crop_outline_black_24x24.png\"); 
+		}
+		
+		// Feedback
+		\$(\"#ucrjphp_upload_feedback\").html(\"<div class='info_small'><p>Crop tool loaded \" + cropIsActive + \"</p></div>\");
+		\$(\"#ucrjphp_upload_feedback\").children().delay(20000).fadeOut(800);
+
+
+		// Activate or deactivate crop
+		if(cropIsActive === '1'){
+			\$('#ucrjphp_img').rcrop({
+				grid : true
+			});
+
+		}
+		else{
+			\$('#ucrjphp_img').rcrop({
+				grid : false
+			});
+		}
+
+	}); // Crop
+
 
 
 });
